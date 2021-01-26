d94774447d15fad384d1aa75d60c781ec254ac6a
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-01-26 00:52:48 -0500

    Add files via upload

--- JS/About.js
+++ JS/About.js
@@ -1,5 +1,5 @@
 var btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
   location.href =
-    "https://drive.google.com/file/d/12ccBJssAx6b7AAARUlLKYKJLyqSbPSFG/view?usp=sharing";
+    "https://drive.google.com/file/d/1HXFZowvwywkI-kvaHqf4nsiOWC3ozb5m/view";
 });
