4eb5c5f90d81ade54b4dbd3e0b76f94419b26665
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-01-26 00:36:34 -0500

    Add files via upload

---
+++ Homepage_DataMining/JS/About.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  location.href =
+    "https://drive.google.com/file/d/1HXFZowvwywkI-kvaHqf4nsiOWC3ozb5m/view";
+});
---
+++ Homepage_DataMining/JS/Contact.js
@@ -0,0 +1,21 @@
+let twitter = document.querySelector(".twitter");
+let facebook = document.querySelector(".facebook");
+let github = document.querySelector(".github");
+let instagram = document.querySelector(".instagram");
+var linkkdn=document.querySelector(".linkdn")
+
+twitter.addEventListener("click", () => {
+  location.href = "https://twitter.com/vijayga90091961";
+});
+facebook.addEventListener("click", () => {
+  location.href = "https://www.facebook.com/vijay.ganesh.7505";
+});
+instagram.addEventListener("click", () => {
+  location.href = "https://www.instagram.com/vijayganesh_vg/?hl=en";
+});
+github.addEventListener("click", () => {
+  location.href = "https://github.com/Vijayganesh3107";
+});
+linkkdn.addEventListener("click",()=>{
+  location.href="https://www.linkedin.com/in/vijay-ganesh-panchapakesan-1267921b6/"
+})
