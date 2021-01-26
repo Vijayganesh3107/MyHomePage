95df6549d87a0600b68a25bbed97237841447657
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-01-26 00:51:31 -0500

    Delete Homepage_DataMining directory

--- Homepage_DataMining/JS/About.js
+++
@@ -1,5 +0,0 @@
-var btn = document.querySelector(".btn");
-btn.addEventListener("click", () => {
-  location.href =
-    "https://drive.google.com/file/d/1HXFZowvwywkI-kvaHqf4nsiOWC3ozb5m/view";
-});
